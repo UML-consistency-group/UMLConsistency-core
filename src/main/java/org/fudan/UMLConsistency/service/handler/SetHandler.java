c9e9ec4015691f05861b02c620a328148806c586
cjhhgs <462072107@qq.com>
2022-04-06 19:34:45 +0800

    temp

--- src/main/java/org/fudan/UMLConsistency/service/handler/SetHandler.java
+++ src/main/java/org/fudan/UMLConsistency/service/handler/SetHandler.java
@@ -1,9 +1,12 @@
 package org.fudan.UMLConsistency.service.handler;
 
+import org.fudan.UMLConsistency.DAO.InstanceStorage;
 import org.fudan.UMLConsistency.service.OptHandler;
 import org.fudan.UMLConsistency.service.OptHandlerType;
 import org.fudan.UMLConsistency.cons.OptType;
+import org.fudan.UMLConsistency.uml.UMLFactory;
 import org.fudan.UMLConsistency.uml.UMLInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,11 +14,30 @@ import org.springframework.stereotype.Service;
  * @date: 2022-04-04 15:41
  * @description: Set操作的具体实现
  */
+/** !set InstanceName.AttributeName := value */
 @Service
 @OptHandlerType(value = OptType.SET)
 public class SetHandler implements OptHandler {
+    @Autowired
+    private InstanceStorage instanceStorage;
+
+    @Autowired
+    private UMLFactory umlFactory;
+
     @Override
     public UMLInstance handleOpt(String operation) {
+        /** 分解参数 */
+        String[] attrs=operation.split(" ");
+        String value = attrs[3];
+        String[] attrs2 = attrs[1].split(".");
+        String instanceName = attrs2[0];
+        String attributeName = attrs2[1];
+
+
+
+        instanceStorage.setAttribute(instanceName,value);
+
+
         return null;
     }
 }
