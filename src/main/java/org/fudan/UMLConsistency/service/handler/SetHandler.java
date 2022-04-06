6d413c58fa2c7b65255be9f39daee70f2c83f8cf
Ln-Yangzl <565213263@qq.com>
2022-04-06 18:57:21 +0800

    添加项目代码说明

--- src/main/java/org/fudan/UMLConsistency/service/handler/SetHandler.java
+++ src/main/java/org/fudan/UMLConsistency/service/handler/SetHandler.java
@@ -1,10 +1,16 @@
 package org.fudan.UMLConsistency.service.handler;
 
+import org.fudan.UMLConsistency.DAO.InstanceStorage;
 import org.fudan.UMLConsistency.service.OptHandler;
 import org.fudan.UMLConsistency.service.OptHandlerType;
 import org.fudan.UMLConsistency.cons.OptType;
 import org.fudan.UMLConsistency.uml.UMLInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: zlyang
@@ -14,6 +20,10 @@ import org.springframework.stereotype.Service;
 @Service
 @OptHandlerType(value = OptType.SET)
 public class SetHandler implements OptHandler {
+
+    @Autowired
+    private InstanceStorage instanceStorage;
+
     @Override
     public UMLInstance handleOpt(String operation) {
         return null;
